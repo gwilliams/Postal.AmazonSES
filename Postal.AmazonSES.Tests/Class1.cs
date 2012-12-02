50bb0f61ce3c8dbf3318ae53afa11be96f222c40
Gareth Williams <gw@gdoubleu.co.uk>
2012-12-02 15:34:15 +0000

    First commit

---
+++ Postal.AmazonSES.Tests/Class1.cs
@@ -0,0 +1,43 @@
+﻿using System.Net.Mail;
+using Amazon.SimpleEmail.Model;
+using FakeItEasy;
+using SubSpec;
+using Xunit;
+
+namespace Postal.AmazonSES.Tests
+{
+    public class AmazonSimpleEmailServiceSpecs
+    {
+        [Specification]
+        public void CanConvertMailMessageToSendEmailRequest()
+        {
+            var emailService = default(AmazonSimpleEmailService);
+            var email = default(Email);
+            var sendEmailRequest = default(SendEmailRequest);
+
+            "Given I have an email".Context(() =>
+                {
+                    var fakeEmailRenderer = A.Fake<IEmailViewRenderer>();
+                    var fakeEmailParser = A.Fake<IEmailParser>();
+                    var fakeAmazonEmailService = A.Fake<Amazon.SimpleEmail.AmazonSimpleEmailService>();
+                    email = new Email("test");
+                    A.CallTo(() => fakeEmailRenderer.Render(email, null)).Returns("");
+                    A.CallTo(() => fakeEmailParser.Parse("", email)).Returns(new MailMessage("sender@test.com","test@test.com","Subject","Body"));
+                    A.CallTo(() => fakeAmazonEmailService.SendEmail(A<SendEmailRequest>.Ignored)).Invokes(x => sendEmailRequest = x.Arguments.Get<SendEmailRequest>(0))
+                     .Returns(new SendEmailResponse());
+
+                    emailService = new AmazonSimpleEmailService(fakeEmailRenderer, fakeEmailParser, fakeAmazonEmailService);
+                });
+
+            "When I create an Amazon SES Request".Do(() => emailService.Send(email));
+
+            "Then the destination address is set".Observation(() => Assert.Equal("test@test.com", sendEmailRequest.Destination.ToAddresses[0]));
+
+            "And the source address is set".Observation(() => Assert.Equal("sender@test.com", sendEmailRequest.Source));
+
+            "And the subject is set".Observation(() => Assert.Equal("Subject", sendEmailRequest.Message.Subject.Data));
+
+            "And the body is set".Observation(() => Assert.Equal("Body", sendEmailRequest.Message.Body.Text.Data));
+        }
+    }
+}
---
+++ Postal.AmazonSES/AmazonSimpleEmailService.cs
@@ -0,0 +1,79 @@
+﻿using System.Linq;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using Amazon.SimpleEmail.Model;
+
+namespace Postal.AmazonSES
+{
+    public class AmazonSimpleEmailService : IEmailService
+    {
+        readonly IEmailViewRenderer _emailViewRenderer;
+        readonly Amazon.SimpleEmail.AmazonSimpleEmailService _simpleEmailService;
+        readonly IEmailParser _emailParser;
+
+        public AmazonSimpleEmailService(IEmailViewRenderer emailViewRenderer)
+        {
+            _emailViewRenderer = emailViewRenderer;
+            _simpleEmailService = Amazon.AWSClientFactory.CreateAmazonSimpleEmailServiceClient();
+            _emailParser = new EmailParser(emailViewRenderer);
+        }
+
+        public AmazonSimpleEmailService(IEmailViewRenderer emailViewRenderer, Amazon.SimpleEmail.AmazonSimpleEmailService amazonSimpleEmailService)
+        {
+            _emailViewRenderer = emailViewRenderer;
+            _simpleEmailService = amazonSimpleEmailService;
+            _emailParser = new EmailParser(emailViewRenderer);
+        }
+
+        public AmazonSimpleEmailService(IEmailViewRenderer emailViewRenderer, IEmailParser emailParser, Amazon.SimpleEmail.AmazonSimpleEmailService amazonSimpleEmailService)
+        {
+            _emailViewRenderer = emailViewRenderer;
+            _simpleEmailService = amazonSimpleEmailService;
+            _emailParser = emailParser;
+        }
+
+        public void Send(Email email)
+        {
+            using (var mailMessage = CreateMailMessage(email))
+            {
+                _simpleEmailService.SendEmail(CreateSendEmailRequest(mailMessage));
+            }
+        }
+
+        public Task SendAsync(Email email)
+        {
+            var taskCompletionSource = new TaskCompletionSource<object>();
+            using (var mailMessage = CreateMailMessage(email))
+            {
+                _simpleEmailService.BeginSendEmail(CreateSendEmailRequest(mailMessage),
+                    e =>
+                        {
+                            var result = _simpleEmailService.EndSendEmail(e);
+                            taskCompletionSource.SetResult(result.SendEmailResult);
+                    }, null);
+            }
+            return taskCompletionSource.Task;
+        }
+
+        public MailMessage CreateMailMessage(Email email)
+        {
+            var rawEmailString = _emailViewRenderer.Render(email);
+            var mailMessage = _emailParser.Parse(rawEmailString, email);
+            return mailMessage;
+        }
+
+        private SendEmailRequest CreateSendEmailRequest(MailMessage mailMessage)
+        {
+            return new SendEmailRequest
+                       {
+                           Destination = new Destination(mailMessage.To.Select(e => e.Address).ToList()),
+                           Source = mailMessage.From.Address,
+                           Message = new Message
+                                         {
+                                             Body = new Body(new Content(mailMessage.Body)),
+                                             Subject = new Content(mailMessage.Subject)
+                                         }
+                       };
+        }
+    }
+}
