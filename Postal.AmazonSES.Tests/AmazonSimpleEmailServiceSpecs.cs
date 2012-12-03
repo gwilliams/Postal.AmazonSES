b51a46f7de3261f82f3d3425d3646b1fadd9ac80
Gareth Williams <gw@gdoubleu.co.uk>
2012-12-03 21:45:43 +0000

    wip - tests for async call

--- Postal.AmazonSES.Tests/AmazonSimpleEmailServiceSpecs.cs
+++ Postal.AmazonSES.Tests/AmazonSimpleEmailServiceSpecs.cs
@@ -1,6 +1,9 @@
-﻿using System.Net.Mail;
+﻿using System;
+using System.Net.Mail;
+using System.Threading.Tasks;
 using Amazon.SimpleEmail.Model;
 using FakeItEasy;
+using FluentAssertions;
 using SubSpec;
 using Xunit;
 
@@ -31,13 +34,52 @@ namespace Postal.AmazonSES.Tests
 
             "When I create an Amazon SES Request".Do(() => emailService.Send(email));
 
-            "Then the destination address is set".Observation(() => Assert.Equal("test@test.com", sendEmailRequest.Destination.ToAddresses[0]));
+            "Then the destination address is set".Observation(() => sendEmailRequest.Destination.ToAddresses[0].ShouldBeEquivalentTo("test@test.com"));
 
-            "And the source address is set".Observation(() => Assert.Equal("sender@test.com", sendEmailRequest.Source));
+            "And the source address is set".Observation(() => sendEmailRequest.Source.ShouldBeEquivalentTo("sender@test.com"));
 
-            "And the subject is set".Observation(() => Assert.Equal("Subject", sendEmailRequest.Message.Subject.Data));
+            "And the subject is set".Observation(() => sendEmailRequest.Message.Subject.Data.ShouldBeEquivalentTo("Subject"));
 
-            "And the body is set".Observation(() => Assert.Equal("Body", sendEmailRequest.Message.Body.Text.Data));
+            "And the body is set".Observation(() => sendEmailRequest.Message.Body.Text.Data.ShouldBeEquivalentTo("Body"));
+        }
+
+        [Specification]
+        public void CanSendAsyncEmail()
+        {
+            var emailService = default(AmazonSimpleEmailService);
+            var email = default(Email);
+            var sendEmailRequest = default(SendEmailRequest);
+            var result = default(object);
+
+            "Given I have an email".Context(() =>
+            {
+                var fakeEmailRenderer = A.Fake<IEmailViewRenderer>();
+                var fakeEmailParser = A.Fake<IEmailParser>();
+                var fakeAmazonEmailService = A.Fake<Amazon.SimpleEmail.AmazonSimpleEmailService>();
+                email = new Email("test");
+                A.CallTo(() => fakeEmailRenderer.Render(email, null)).Returns("");
+                A.CallTo(() => fakeEmailParser.Parse("", email)).Returns(new MailMessage("sender@test.com", "test@test.com", "Subject", "Body"));
+
+                A.CallTo(
+                    () =>
+                    fakeAmazonEmailService.BeginSendEmail(A<SendEmailRequest>.Ignored,
+                                                          (s) => A.CallTo(() => fakeAmazonEmailService.EndSendEmail(s))
+                                                          .Returns(new SendEmailResponse
+                                                                       {
+                                                                           SendEmailResult = new SendEmailResult
+                                                                                                 {
+                                                                                                     MessageId = "123"
+                                                                                                 }
+                                                                       }),
+                                                          null));
+
+
+                emailService = new AmazonSimpleEmailService(fakeEmailRenderer, fakeEmailParser, fakeAmazonEmailService);
+            });
+
+            "When I create an Amazon SES Request".Do(() => emailService.SendAsync(email));
+
+            "Then something".Observation(() => { var x = 1; });
         }
     }
 }
